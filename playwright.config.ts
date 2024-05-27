cf8f50b63d126ce9a35e3437980e321024dadf53
Manolo Carrasco <manolo@vaadin.com>
2024-05-27 14:34:29 +0200

    chore: reuseExistingServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   webServer: {
     command: "./mvnw -B -ntp -Dspring-boot.run.jvmArguments='-Dvaadin.launch-browser=false'",
     url: "http://127.0.0.1:8080",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     stdout: "pipe",
     stderr: "pipe",
   },  
