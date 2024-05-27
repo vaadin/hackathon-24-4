c80deecb6671049443f7963dd1877e2a73c2dd08
Stefan Uebe <stefan@vaadin.com>
2024-05-27 07:43:19 +0200

    some comments

--- src/main/java/com/example/application/fullcalendar/FullCalendarConnector.java
+++ src/main/java/com/example/application/fullcalendar/FullCalendarConnector.java
@@ -7,13 +7,32 @@ import com.example.application.fullcalendar.model.Entry;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+/**
+ * This interface is the main entry point for the developer using the Hilla integration of the FC. The FC Endpoint
+ * will inject an instance of this interface and use its methods to interact with the client, e.g. by providing
+ * calendar items to show.
+ * <p/>
+ * It is recommended to use an EntryProvider, but you may also override the fetch methods, if preferred.
+ */
 public interface FullCalendarConnector {
+    /**
+     * Returns the entry provider to be used.
+     * @return entry provider
+     */
     EntryProvider<Entry> getEntryProvider();
 
+    /**
+     * Returns the entries matching the given entry query. Delegates the call to the entry provider by default
+     * @return entries
+     */
     default Stream<Entry> fetchEntries(EntryQuery entryQuery) {
         return getEntryProvider().fetch(entryQuery);
     }
 
+    /**
+     * Returns the entry matching the given id or empty. Delegates the call to the entry provider by default
+     * @return entry or empty
+     */
     default Optional<Entry> fetchEntry(String id) {
         return getEntryProvider().fetchById(id);
     }
